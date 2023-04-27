117e1231c09653e1e16570363af45aac7044b73f
IvanStepus <vanya-stepys@mail.ru>
2023-04-27 22:27:13 +0300

    Improved functionality and fixed errors

--- utills.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-def get_bitcoin_price():
-    resp = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=Bitcoin&vs_currencies=USD').json
-    bitcoin_price = resp.get('bitcoin').get('usd')
-    return bitcoin_price
