50c07ca6e47d84e5531a76d68adb52c0a9ddb67a
IvanStepus <vanya-stepys@mail.ru>
2023-05-20 23:07:25 +0300

    create new html files and db

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
 from flask import Flask, render_template
 from time import ctime
 from utils import get_bitcoin_price, get_news
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    psw = db.Column(db.String(500), nullable=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<users {self.id}"
+
+
+class Profiles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=True)
+    old = db.Column(db.Integer)
+    city = db.Column(db.String(100))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f"<users {self.id}"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="Glavnaya")
+
+
+@app.route('/register', methods=('POST', 'GET'))
+def register():
+    return render_template('register_page.html', title="Registration")
+
 
 
 @app.route('/news')
@@ -40,4 +77,4 @@ def exit_page():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
